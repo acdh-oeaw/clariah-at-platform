526bfb629311ab8f2bcd3bdb3e265a6da7cbf2c6
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2024-12-19 13:33:26 +0000

    feat: create new collections and singletons

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -4,20 +4,61 @@ import { config } from "@keystatic/core";
 import { Logo } from "@/components/logo";
 import { env } from "@/config/env.config";
 import { locales } from "@/config/i18n.config";
-import { createDocumentation } from "@/lib/keystatic/collections";
-import { createMetadata, createNavigation } from "@/lib/keystatic/singletons";
+import {
+	createConsortium,
+	createDocumentation,
+	createEvents,
+	createNews,
+	createPages,
+	createProjects,
+} from "@/lib/keystatic/collections";
+import {
+	createEventsOverview,
+	createIndexPage,
+	createMetadata,
+	createNavigation,
+	createNewsOverview,
+	createProjectsOverview,
+} from "@/lib/keystatic/singletons";
 
 export default config({
 	collections: {
+		[withI18nPrefix("consortium", "de")]: createConsortium("de"),
+		[withI18nPrefix("consortium", "en")]: createConsortium("en"),
+
+		[withI18nPrefix("events", "de")]: createEvents("de"),
+		[withI18nPrefix("events", "en")]: createEvents("en"),
+
+		[withI18nPrefix("news", "de")]: createNews("de"),
+		[withI18nPrefix("news", "en")]: createNews("en"),
+
+		[withI18nPrefix("projects", "de")]: createProjects("de"),
+		[withI18nPrefix("projects", "en")]: createProjects("en"),
+
+		[withI18nPrefix("pages", "de")]: createPages("de"),
+		[withI18nPrefix("pages", "en")]: createPages("en"),
+
 		[withI18nPrefix("documentation", "de")]: createDocumentation("de"),
 		[withI18nPrefix("documentation", "en")]: createDocumentation("en"),
 	},
 	singletons: {
+		[withI18nPrefix("index-page", "de")]: createIndexPage("de"),
+		[withI18nPrefix("index-page", "en")]: createIndexPage("en"),
+
 		[withI18nPrefix("metadata", "de")]: createMetadata("de"),
 		[withI18nPrefix("metadata", "en")]: createMetadata("en"),
 
 		[withI18nPrefix("navigation", "de")]: createNavigation("de"),
 		[withI18nPrefix("navigation", "en")]: createNavigation("en"),
+
+		[withI18nPrefix("events-overview", "de")]: createEventsOverview("de"),
+		[withI18nPrefix("events-overview", "en")]: createEventsOverview("en"),
+
+		[withI18nPrefix("news-overview", "de")]: createNewsOverview("de"),
+		[withI18nPrefix("news-overview", "en")]: createNewsOverview("en"),
+
+		[withI18nPrefix("projects-overview", "de")]: createProjectsOverview("de"),
+		[withI18nPrefix("projects-overview", "en")]: createProjectsOverview("en"),
 	},
 	storage:
 		env.NEXT_PUBLIC_KEYSTATIC_MODE === "github" &&
@@ -42,8 +83,32 @@ export default config({
 			name: "ACDH-CH",
 		},
 		navigation: {
-			Documentation: locales.map((locale) => {
-				return withI18nPrefix("documentation", locale);
+			HomePage: locales.map((locale) => {
+				return withI18nPrefix("index-page", locale);
+			}),
+			Events: locales.map((locale) => {
+				return withI18nPrefix("events", locale);
+			}),
+			"Events Overview": locales.map((locale) => {
+				return withI18nPrefix("events-overview", locale);
+			}),
+			News: locales.map((locale) => {
+				return withI18nPrefix("news", locale);
+			}),
+			"News Overview": locales.map((locale) => {
+				return withI18nPrefix("news-overview", locale);
+			}),
+			Projects: locales.map((locale) => {
+				return withI18nPrefix("projects", locale);
+			}),
+			"Projects Overview": locales.map((locale) => {
+				return withI18nPrefix("projects-overview", locale);
+			}),
+			Pages: locales.map((locale) => {
+				return withI18nPrefix("pages", locale);
+			}),
+			Consortium: locales.map((locale) => {
+				return withI18nPrefix("consortium", locale);
 			}),
 			Navigation: locales.map((locale) => {
 				return withI18nPrefix("navigation", locale);
