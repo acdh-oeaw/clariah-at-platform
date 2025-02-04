6f07b3ce6856b7693afabd104142e1d6616968cb
stefanprobst <stefanprobst@posteo.de>
2025-02-04 15:11:08 +0100

    docs: update content collection schemas

--- docs/content-collections.ts
+++ docs/content-collections.ts
@@ -1,66 +1,119 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
+type IsoDateString = string;
+type MdxInlineContent = string;
+type MdxContent = string;
+
 interface Event {
-	title: string;
-	// publication date
-	date: string;
-	image: string;
-	location: string;
-	host: Array<Person | Organisation>;
-	speaker: Array<Person>;
-	summary: string;
-	eventDateStart: string;
-	eventLocation: string;
-	eventDateEnd?: string;
-	shortTitle?: string;
-	links?: Array<{
-		label: string;
-		url: string;
-	}>;
+  title: string;
+  publicationDate: IsoDateString;
+  image: {
+    src: string;
+    caption?: MdxInlineContent;
+  };
+  summary: {
+    content: string;
+    title?: string;
+  };
+  startDate: IsoDateString;
+  endDate?: IsoDateString;
+  location: string;
+  hosts: Array<Person | Organisation>;
+  speakers: Array<Person>;
+  links: Array<{
+    label: string;
+    href: string;
+  }>;
+  attachments: Array<{
+    label: string;
+    href: string;
+  }>;
+  content: MdxContent;
 }
 
 interface News {
-	title: string;
-	// publication date
-	date: string;
-	image: string;
-	summary: string;
-	shortTitle?: string;
+  title: string;
+  publicationDate: IsoDateString;
+  image: {
+    src: string;
+    caption?: MdxInlineContent;
+  };
+  summary: {
+    content: string;
+    title?: string;
+  };
+	links: Array<{
+    label: string;
+    href: string;
+  }>;
+  attachments: Array<{
+    label: string;
+    href: string;
+  }>;
+  content: MdxContent;
 }
 
 interface Organisation {
-	name: string;
-	image: string;
-	description: string;
-	affiliation: Array<Organisation>;
-	consortiumStatus: "member" | "observer";
+  name: string;
+  image: {
+    src: string;
+    caption?: MdxInlineContent;
+  };
+  affiliations: Array<Organisation>;
+  consortiumStatus: "member" | "observer";
+  description: MdxContent;
 }
 
 interface Page {
-	title: string;
-	image?: string;
-	summary?: string;
+  title: string;
+  image: {
+    src: string;
+    caption?: MdxInlineContent;
+  };
+  summary: {
+    content: string;
+    title?: string;
+  };
+  content: MdxContent;
 }
 
 interface Person {
-	name: string;
-	image: string;
-	description: string;
-	affiliation: Array<Organisation>;
-	links: Array<SocialMediaLink>;
-	contact: Array<Person>;
+  name: string;
+  image: {
+    src: string;
+    caption?: MdxInlineContent;
+  };
+  affiliations: Array<Organisation>;
+  links: Array<SocialMediaLink>;
+  contact: Array<Person>;
+  description: MdxContent;
 }
 
 interface Project {
-	title: string;
-	summary: string;
-	shortTitle?: string;
-	startDate?: string;
-	endDate?: string;
-	image: string;
+  title: string;
+  image: {
+    src: string;
+    caption?: MdxInlineContent;
+  };
+  summary: {
+    content: string;
+    title?: string;
+  };
+  startDate?: IsoDateString;
+  endDate?: IsoDateString;
 }
 
 interface SocialMediaLink {
-	type: "Bluesky" | "Instagram" | "LinkedIn" | "Mastodon" | "ORCID";
-	url: string;
+  type:
+    | "bluesky"
+    | "email"
+    | "facebook"
+    | "instagram"
+    | "linked-in"
+    | "mastodon"
+    | "orcid"
+    | "twitter"
+    | "website"
+    | "youtube";
+  href: string;
 }
